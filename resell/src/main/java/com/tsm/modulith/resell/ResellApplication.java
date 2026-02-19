d91a909d579340b5d027349eb4d879c88a824f06
SimonePiccirilli <simone.piccirilli91@gmail.com>
2026-02-19 17:34:27 +0100

    first init

---
+++ resell/src/main/java/com/tsm/modulith/resell/ResellApplication.java
@@ -0,0 +1,13 @@
+package com.tsm.modulith.resell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResellApplication.class, args);
+	}
+
+}
---
+++ resell/src/test/java/com/tsm/modulith/resell/ResellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsm.modulith.resell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
